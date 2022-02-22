5a196b076a32d86edd51f43f6dd497877a3cda79
Mayooonaiselol <97210788+Mayooonaiselol@users.noreply.github.com>
2022-02-22 21:55:27 +0530

    Delete lua/vanilla directory

--- lua/vanilla/plugins/lightbulb.lua
+++
@@ -1,4 +0,0 @@
-local _2afile_2a = ".config/nvim/fnl/vanilla/plugins/lightbulb.fnl"
-local _local_1_ = require("nvim-lightbulb")
-local update_lightbulb = _local_1_["update_lightbulb"]
-return update_lightbulb({sign = {enabled = true, priority = 10}, float = {enabled = false, text = "\240\159\146\161"}, virtual_text = {enabled = false, text = "\240\159\146\161", hl_mode = "replace"}, status_text = {enabled = false, text = "\240\159\146\161", text_unavailable = ""}})
--- lua/vanilla/plugins/neorg.lua
+++
@@ -1,8 +0,0 @@
-local _2afile_2a = ".config/nvim/fnl/vanilla/plugins/neorg.fnl"
-local _local_1_ = require("neorg")
-local setup = _local_1_["setup"]
-setup({load = {["core.defaults"] = {}, ["core.norg.concealer"] = {}, ["core.norg.qol.toc"] = {}, ["core.norg.completion"] = {config = {engine = "nvim-cmp"}}, ["core.norg.dirman"] = {config = {workspaces = {main = "~/org/neorg"}, autodetect = true, autochdir = true}}}})
-local parser_config = (require("nvim-treesitter.parsers")).get_parser_configs()
-parser_config.norg_meta = {install_info = {url = "https://github.com/nvim-neorg/tree-sitter-norg-meta", files = {"src/parser.c"}, branch = "main"}}
-parser_config.norg_table = {install_info = {url = "https://github.com/nvim-neorg/tree-sitter-norg-table", files = {"src/parser.c"}, branch = "main"}}
-return nil
