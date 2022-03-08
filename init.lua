f3b63751323483af1aa23b5b5fbc0bafe12aa26a
Mayooonaiselol <anshgupta3552@gmail.com>
2022-03-08 12:16:40 +0530

    cleanup

--- init.lua
+++ init.lua
@@ -1,16 +1,9 @@
-local execute = vim.api.nvim_command
-local fn = vim.fn
-
-local pack_path = fn.stdpath("data") .. "/site/pack"
-local fmt = string.format
-
-function ensure (user, repo)
-  -- Ensures a given github.com/USER/REPO is cloned in the pack/packer/start directory.
-  local install_path = fmt("%s/packer/start/%s", pack_path, repo)
-  if fn.empty(fn.glob(install_path)) > 0 then
-    execute(fmt("!git clone https://github.com/%s/%s %s", user, repo, install_path))
-    execute(fmt("packadd %s", repo))
-  end
+function ensure(user, repo)
+    local install_path = string.format("%s/packer/start/%s", vim.fn.stdpath("data") .. "/site/pack", repo, repo)
+        if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+        vim.api.nvim_command(string.format("!git clone https://github.com/%s/%s %s", user, repo, install_path))
+        vim.api.nvim_command(string.format("packadd %s", repo))
+    end
 end
 
 -- Bootstrap essential plugins required for installing and loading the rest.
@@ -22,7 +15,4 @@ ensure("lewis6991", "impatient.nvim")
 require("impatient")
 
 -- Enable Aniseed's automatic compilation and loading of Fennel source code.
-vim.g["aniseed#env"] = {
-  module = "vanilla.init",
-  compile = true
-}
+vim.g["aniseed#env"] = {module = "vanilla.init"}
