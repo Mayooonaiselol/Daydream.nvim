7f3497f0354cb2e22f6f8b597ae82b52a24f8dfa
Mayooonaiselol <anshgupta3552@gmail.com>
2022-03-08 12:08:09 +0530

    cleanup

--- init.lua
+++ init.lua
@@ -26,7 +26,3 @@ vim.g["aniseed#env"] = {
   module = "vanilla.init",
   compile = true
 }
-
-vim.cmd [[
-  au TermOpen term://* setlocal nonumber norelativenumber signcolumn=no | setfiletype terminal
-]]
